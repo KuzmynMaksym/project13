b7f1c7d57aabfd1c7e5a1ca0183750f9afdd0c7a
KuzmynMaksym <144415346+KuzmynMaksym@users.noreply.github.com>
2023-10-26 11:10:25 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, y;
+    int sum = 0;
+    cout << "Enter the number: ";
+    cin >> x;
+    for (; x > 0;) {
+        y = x % 10;
+        sum += y;
+        x /= 10;
+    }
+    cout << sum << endl;
+    return 0;
+}
